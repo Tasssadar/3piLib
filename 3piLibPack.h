340b12379e36501b242e3ce43a5351da2da412ca
Tassadar <vbocek@gmail.com>
2011-09-21 17:32:25 +0200

    [15] Port IR sensor calibration from 3pi lib

--- 3piLibPack.h
+++ 3piLibPack.h
@@ -1,4 +1,4 @@
-#define PI_LIB_VERSION 14
+#define PI_LIB_VERSION 15
 
 #ifndef PI_LIB_COMMON
 #define PI_LIB_COMMON
@@ -454,13 +454,17 @@ ISR(TIMER1_COMPA_vect)
 // Current setting is 4800mV
 #define LOW_BATTERY 654
 
+#define PI_GRND_SENSOR_COUNT 5
+
 struct ground_sensors_t
 {
     uint16_t value[6];
 };
 
 volatile struct ground_sensors_t g_sensors;
-uint16_t g_threshold = 512;
+
+volatile uint16_t g_calibratedMinimum[PI_GRND_SENSOR_COUNT];
+volatile uint16_t g_calibratedMaximum[PI_GRND_SENSOR_COUNT];
 
 ISR(ADC_vect)
 {
@@ -494,28 +498,6 @@ ISR(ADC_vect)
     ADCSRA |= (1<<ADSC);
 }
 
-void init_sensors()
-{
-    DIDR0 = (1<<ADC0D)|(1<<ADC1D)|(1<<ADC2D)|(1<<ADC3D)|(1<<ADC4D)|(1<<ADC5D);
-    ADCSRA = (1<<ADEN)|(1<<ADSC)|(1<<ADIF)|(1<<ADIE)|(1<<ADPS2)|(1<<ADPS1)|(1<<ADPS0);
-    ADCSRB = 0;
-    
-    ADMUX = (1<<REFS0);
-    PORTC |= (1 << 0)|(1 << 1)|(1 << 2)|(1 << 3)|(1 << 4)|(1 << 5);
-    
-    g_sensors.value[0] = 1024;
-    g_sensors.value[1] = 1024;
-    g_sensors.value[2] = 1024;
-    g_sensors.value[3] = 1024;
-    g_sensors.value[4] = 1024;
-    g_sensors.value[5] = 1024;
-}
-
-void clean_sensors()
-{
-    ADCSRA = 0;
-}
-
 inline int16_t getSensorValue(uint8_t index, bool threshold = true)
 {
     cli();
@@ -526,25 +508,96 @@ inline int16_t getSensorValue(uint8_t index, bool threshold = true)
         cli();
     }
     int16_t res = g_sensors.value[index];
-    if(threshold) res -= g_threshold;
     sei();
     nop();
+    
+    if(threshold)
+    {
+        uint16_t denominator = g_calibratedMaximum[index] - g_calibratedMinimum[index];
+
+        int16_t val = res;
+        if(denominator)
+            val = int32_t(res - g_calibratedMinimum[index])*1000/denominator;
+        if(val < 0)
+            res = 0;
+        else if(val > 1024)
+            res = 1024;
+        else
+            res = val;
+    }
 
     return res; 
 }
 
-inline void calibrate_sensors()
+void resetCalibration()
 {
-    uint32_t avg = 0;
-    for(uint8_t i = 0; i < 5; ++i)
-        avg += getSensorValue(i);
-    
-    g_threshold = (uint16_t) (avg / 5);
+    for(uint8_t i = 0; i < PI_GRND_SENSOR_COUNT; ++i)
+    {
+        g_calibratedMaximum[i] = 0;
+        g_calibratedMinimum[i] = 1024;
+    }
+}
+
+void calibrate_sensors()
+{
+    uint8_t i;
+    uint16_t sensor_value;
+    uint16_t max_sensor_values[PI_GRND_SENSOR_COUNT];
+    uint16_t min_sensor_values[PI_GRND_SENSOR_COUNT];
+
+    for(uint8_t j = 0; j<10; ++j)
+    {
+        for(i = 0; i<PI_GRND_SENSOR_COUNT; ++i)
+        {
+            sensor_value = getSensorValue(i, false);
+            // set the max we found THIS time
+            if(j == 0 || max_sensor_values[i] < sensor_value)
+                max_sensor_values[i] = sensor_value;
+
+            // set the min we found THIS time
+            if(j == 0 || min_sensor_values[i] > sensor_value)
+                min_sensor_values[i] = sensor_value;
+        }
+    }
+
+    // record the min and max calibration values
+    for(i = 0; i < PI_GRND_SENSOR_COUNT; ++i)
+    {
+        if(min_sensor_values[i] > g_calibratedMaximum[i])
+            g_calibratedMaximum[i] = min_sensor_values[i];
+        if(max_sensor_values[i] < g_calibratedMinimum[i])
+            g_calibratedMinimum[i] = max_sensor_values[i];
+    }
 }
 
 inline uint16_t getBatteryVoltage()
 {
     return (((uint32_t(getSensorValue(5, false))*5000+511)/1023)*3+1)/2;
+}
+
+
+void init_sensors()
+{
+    DIDR0 = (1<<ADC0D)|(1<<ADC1D)|(1<<ADC2D)|(1<<ADC3D)|(1<<ADC4D)|(1<<ADC5D);
+    ADCSRA = (1<<ADEN)|(1<<ADSC)|(1<<ADIF)|(1<<ADIE)|(1<<ADPS2)|(1<<ADPS1)|(1<<ADPS0);
+    ADCSRB = 0;
+
+    ADMUX = (1<<REFS0);
+    PORTC |= (1 << 0)|(1 << 1)|(1 << 2)|(1 << 3)|(1 << 4)|(1 << 5);
+
+    g_sensors.value[0] = 1024;
+    g_sensors.value[1] = 1024;
+    g_sensors.value[2] = 1024;
+    g_sensors.value[3] = 1024;
+    g_sensors.value[4] = 1024;
+    g_sensors.value[5] = 1024;
+
+    resetCalibration();
+}
+
+void clean_sensors()
+{
+    ADCSRA = 0;
 }
 
 #endif
